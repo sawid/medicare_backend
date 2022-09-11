af90f55c89d86fa1a7f0ba5a4fe22bbfe58404b7
sawid <zasawidza@gmail.com>
2022-09-11 16:02:45 +0700

    test post path

--- main.go
+++ main.go
@@ -18,6 +18,11 @@ func main() {
 			"message": "Hello World! path",
 		})
 	})
+	r.POST("/test_path_post", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World! path",
+		})
+	})
 	r.Run()
 
 }
